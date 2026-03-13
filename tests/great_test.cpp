d1f62b7497facf47f84a99d19279c7168e231cf2
ilsr0m <ilsrom@proton.me>
2026-03-13 22:40:37 +0300

    Added test for list_insert

--- tests/great_test.cpp
+++ tests/great_test.cpp
@@ -2,11 +2,29 @@
 
 extern "C"{
     #include "list_c.h"
+
+    typedef struct TestPoint{
+        double x;
+        double y;
+    }test_point_t;
 }
 
 // for append, prepend functions
 int test_integers[10] = {9, 2, -1, 5, 7, 0, 4, 5, 3, 1};
 double test_doubles[10] = {-0.6, 1.7, 15.02, 2.3, 6.33, -19.29, 0.5, 5.11, 3.93, 24.0};
+
+test_point_t test_points[10] = {
+    { 3.1, -6.22 },
+    { 0.0, 9.7 },
+    { 1.2, -0.2 },
+    { 7.3, -6.22 },
+    { -6.1, -9.09 },
+    { 9.1, 15.15 },
+    { 1.11, -6.22 },
+    { 3.87, 88.14 },
+    { 4.44, -14.87 },
+    { 2.28, 27.88 }
+};
 
 TEST(ListTest, CreateTest){
     list_t *list = list_create(sizeof(int));
@@ -124,6 +142,58 @@ TEST(ListTest, ClearTest){
     EXPECT_EQ(list->tail, nullptr);
     EXPECT_EQ(list->size, 0); 
     EXPECT_EQ(list->item_size, 0); 
+
+    list_delete(&list);
+    EXPECT_EQ(list, nullptr);
+}
+
+TEST(ListTest, InsertTest){
+    list_t *list = list_create(sizeof(test_point_t));
+    int result = 0;
+
+    // fill list with values to make it not empty
+    for(int i = 0; i < 10; i++)
+        list_append(list, &test_points[i]);
+
+    printf("a1\n");
+    // put it as first item
+    test_point_t p0 = {2.0, 2.0};
+    result = list_insert(list, &p0, 0);
+    
+    EXPECT_NE(list, nullptr);
+    EXPECT_EQ((*(test_point_t*)list->head->item).x, p0.x); 
+    EXPECT_EQ((*(test_point_t*)list->head->item).y, p0.y); 
+    EXPECT_EQ(result, 0); 
+    EXPECT_EQ(list->size, 11);
+    
+    // put it as last item
+    test_point_t p1 = {4.0, 4.0};
+    result = list_insert(list, &p1, list->size);
+    EXPECT_NE(list, nullptr);
+    EXPECT_EQ((*(test_point_t*)list->tail->item).x, p1.x); 
+    EXPECT_EQ((*(test_point_t*)list->tail->item).y, p1.y); 
+    EXPECT_EQ(result, 0); 
+    EXPECT_EQ(list->size, 12);
+
+    // put it in the middle
+    test_point_t p3 = {5.55, 5.55};
+    int position = 7;
+    result = list_insert(list, &p3, position);
+    node_t *list_iter = list->head;
+    int count = 0;
+    while (list_iter != nullptr)
+    {
+        if(position == count) 
+            break;
+        count++;
+        list_iter = list_iter->next;
+    }
+
+    EXPECT_NE(list, nullptr); 
+    EXPECT_EQ((*(test_point_t*)list_iter->item).x, p3.x); 
+    EXPECT_EQ((*(test_point_t*)list_iter->item).y, p3.y);  
+    EXPECT_EQ(result, 0); 
+    EXPECT_EQ(list->size, 13);
 
     list_delete(&list);
     EXPECT_EQ(list, nullptr);
